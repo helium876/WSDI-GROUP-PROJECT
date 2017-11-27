eead02d0465912a5ec33b71ee4a6ae486f24cce3
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-11-27 12:13:36 -0500

    Extracted the rar file that Gooden pushed.

---
+++ contacts.php
@@ -0,0 +1,112 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>MI CASA | Contacts</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<style type="text/css">.bg{behavior:url("js/PIE.htc");}</style>
+<![endif]-->
+</head>
+<body id="page6">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <h1><a href="index.php" id="logo"></a></h1>
+      <div class="wrapper">
+        <ul id="icons">
+          <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
+        </ul>
+      </div>
+      <nav>
+        <ul id="menu">
+          <li><a href="index.php">home Page</a></li>
+          <li><a href="">Buying </a></li>
+          <li><a href="">Selling </a></li>
+          <li><a href="">Renting </a></li>
+          <li id="menu_active" class="end"><a href="contacts.html">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- / header -->
+  </div>
+</div>
+<!-- content -->
+<div class="body2">
+  <div class="main">
+    <section id="content">
+      <div class="wrapper">
+        <article class="col1">
+          <div class="pad2">
+            <h2 class="pad_bot1 pad_top1">Contact Form</h2>
+            <form id="ContactForm" action="#">
+              <div>
+                <div  class="wrapper"> <span>Your Name:</span>
+                  <input type="text" class="input" >
+                </div>
+                <div  class="wrapper"> <span>Your E-mail:</span>
+                  <input type="text" class="input" >
+                </div>
+                <div  class="textarea_box"> <span>Your Message:</span>
+                  <textarea name="textarea" cols="1" rows="1"></textarea>
+                </div>
+                <span>&nbsp;</span> <a href="#" class="button">Clear</a> <a href="#" class="button">Send</a> </div>
+            </form>
+          </div>
+       
+      </div>
+      <div class="wrapper">
+        <article class="col1">
+          <div class="pad2">
+            <h2 class="pad_bot1">Property info</h2>
+            <p class="pad_bot1"><strong>Property, in the abstract, is what belongs to or with something, 
+			whether as an attribute or as a component of said thing. In the context of this article, 
+			it is one or more components (rather than attributes), whether physical or incorporeal, 
+			of a person's estate; or so belonging to, as in being owned by, a person or jointly a group of people
+			or a legal entity like a corporation or even a society. (With that meaning, the word property is uncountable
+			and so is not used with an indefinite article or as a plural.) Depending on the nature of the property, 
+			an owner of property has the right to consume, alter, share, redefine, rent, mortgage, pawn, sell, exchange, transfer, give away or destroy it.</div>
+          <div class="pad_left1">
+            <div class="wrapper pad_bot3">
+              
+        <article class="col2">
+          <div class="pad1">
+            <h3>MI CASA Contacts</h3>
+            <div class="address"> 320 Old Hope Road,<br>
+              Saint Andrew, Kingston 20.<br>
+              <span> Freephone:<br>
+              Telephone:<br>
+              E-mail: </span> +1 876 559 6580<br>
+              +1 876 603 6035<br>
+              <a href="#">mail@MI CASA.org</a> </div>
+            <h3 class="pad_top1">MI CASA MISSION</h3>
+            <p>Providing a home for everyone, the rich, poor and less fortunate we consider all mi casa making homes a reality.</p>
+             </div>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
+<!-- / content -->
+<div class="body4">
+  <div class="main">
+    <!-- footer -->
+    <footer> <span class="call">For your design call: <span>1-876-477-2117</span></span> Copyright &copy; <a href="#">MI CASA 2017</a> All Rights Reserved<br>
+      Design by <a target="_blank" href=""> Mikhail Gooden Helium</a>
+      
+    </footer>
+    <!-- / footer -->
+  </div>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,250 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>MI CASA</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+
+<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+
+<script type="text/javascript" src="engine0/jquery.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<style type="text/css">.bg{behavior:url("js/PIE.htc");}</style>
+
+	
+
+	
+
+
+
+<![endif]-->
+</head>
+<body id="page1">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <h1><a href="index.php" id="logo"></a></h1>
+      <div class="wrapper">
+        <ul id="icons">
+          <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
+        </ul>
+      </div>
+      <nav>
+        <ul id="menu">
+          <li id="menu_active"><a href="index.html">Home Page</a></li>
+          <li><a href="">Buying </a></li>
+          <li><a href="">Selling </a></li>
+          <li><a href="">Renting </a></li>
+          <li><a href="contacts.php">contact us</a></li>
+         
+        </ul>
+      </nav>
+    </header>
+    <!-- / header -->
+  </div>
+</div>
+<!-- content -->
+<div class="body2">
+  <div class="main">
+    <section id="content">
+      <div class="wrapper">
+        <article class="col1">
+         
+		 <!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
+
+	<div id="wowslider-container0">
+
+	<div class="ws_images"><ul>
+		<li><img src="data0/images/M1 (1).jpg" alt="p-7"  title="Build your Dream" id="wows0_0"/></li>
+		<li><img src="data0/images/M1 (2).jpg" alt="p-8" title="shelter your family from the rain" id="wows0_1"/></li>
+		<li><a href="data0/images/M1 (3).jpg"><img src="data0/images/M1 (3).jpg" alt="full screen slider" title="Dreams become reality" id="wows0_2"/></a></li>
+		<li><img src="data0/images/M1 (4).jpg" alt="slide1" title="In the comfort of your own home" id="wows0_3"/></li>
+	</ul></div>
+			
+				
+	<div class="ws_bullets"><div>
+		<a href="#" title="Build your Dream"><img src="data0/tooltips/p7.jpg" alt="Build your Dream"/>1</a>
+		<a href="#" title="p-8"><img src="data0/tooltips/M1 (2).jpg" alt="p-8"/>2</a>
+		<a href="#" title="slide"><img src="data0/tooltips/M1 (1).jpg" alt="slide"/>3</a>
+		<a href="#" title="slide1"><img src="data0/tooltips/M1 (3).jpg" alt="slide1"/>4</a>
+	</div></div><span class="wsl"><a href=""></a> </span>
+
+	<div class="ws_shadow"></div>
+
+	</div>	
+
+	<script type="text/javascript" src="engine0/wowslider.js"></script>
+
+	<script type="text/javascript" src="engine0/script.js"></script>
+
+	<!-- End WOWSlider.com BODY section --></article>
+        <article class="col2">
+          <form id="form_1" action="#" method="post">
+            <div class="pad1">
+              <h3>Find Your Property</h3>
+              <div class="row"> City &amp; /Town:<br>
+                <input type="text" class="input">
+              </div>
+              <div class="row_select"> Search Area:<br>
+                <input type="text" class="input">
+              </div>
+              <div class="row_select">
+                <div class="cols"> Price Range:<br>
+                  <select>
+                    <option>&nbsp;</option>
+                    <option>4mil</option>
+                    <option>7mil</option>
+                  </select>
+                </div>
+                <div class="cols pad_left1"> to:<br>
+                  <select>
+                    <option>&nbsp;</option>
+                    <option>7mil</option>
+                    <option>15mil</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row_select">
+                <div class="cols"> Bedroom(s):<br>
+                  <select>
+                    <option>&nbsp;</option>
+                    <option>1-2</option>
+                    <option>3+</option>
+                  </select>
+                </div>
+                <div class="cols pad_left1"> Bathroom(s):<br>
+                  <select>
+                    <option>&nbsp;</option>
+                    <option>1</option>
+                    <option>2+</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row_select pad_bot1">
+                <div class="cols"> Rent/Sale:<br>
+                  <select>
+                    <option>&nbsp;</option>
+                    <option>Rent</option>
+                    <option>Sale</option>
+                  </select>
+                </div>
+                <div class="cols pad_left1"> <a href="#" class="button">Proposals</a> </div>
+              </div>
+              Know exactly what you want? <br>
+              Try our <a href="#">Advanced Search</a> </div>
+          </form>
+        </article>
+      </div>
+      <div class="wrapper">
+        <article class="col1">
+          <div class="pad_left1">
+            <h2 class="pad_bot1">Buyers. Sellers. Proprietors. Agents.</h2>
+            <div class="wrapper">
+              <article class="cols">
+                <h4 class="img1">Selling</h4>
+                <p class="pad_bot1"><strong class="color1"> MI CASA is your go-to partner once you’re ready to buy, sell, lease  <br>
+                  or rent a home or commercial property</strong></p>
+                <p class="pad_bot2"> We’ll also take care of your valuations so you get the best value, and property management</p>
+                <a href="#" class="button">Read more</a> </article>
+              <article class="cols pad_left1">
+                <h4 class="img2">Investing</h4>
+                <p class="pad_bot1"><strong class="color1">For most of us, our home and our business places are at the centre of our lives<br>
+                  Real Estate business</strong></p>
+                <p class="pad_bot2"> they’re where we grow together, and where memories are made and kept</p>
+                <a href="#" class="button">Read more</a> </article>
+              <article class="cols pad_left1">
+                <h4 class="img3">Renting</h4>
+                <p class="pad_bot1"><strong class="color1"> we work with you to ensure they’re the right fit – and investment – for you</strong></p>
+                <p class="pad_bot2"> <a href="index.html" class="color2">Main</a>, <a href="buying.html" class="color2">Buying</a>, <a href="selling.html" class="color2">Selling</a>, <a href="renting.html" class="color2">Renting</a>, <a href="finance.html" class="color2">Finance</a>, <a href="contacts.html" class="color2">Contacts</a> </p>
+                <a href="#" class="button">Read more</a> </article>
+            </div>
+          </div>
+        </article>
+        <article class="col2">
+          <div class="pad1">
+            <h3>Special Offers</h3>
+            <ul class="list1">
+              <li><a href="#">Home Loan Offer</a></li>
+              <li><a href="#">Free Calculators</a></li>
+              <li><a href="#">Free Loan Tools</a></li>
+              <li><a href="#">Value Your Home</a></li>
+              <li><a href="#">Recently Sold Properties</a></li>
+              <li><a href="#">Suburb Statistics</a></li>
+              <li><a href="#">Compare Property Prices</a></li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
+<div class="body3">
+  <div class="main">
+    <section id="content2">
+      <div class="wrapper">
+        <article class="col1">
+          <div class="pad2">
+            <h2>Recently Added Properties</h2>
+            <div class="wrapper pad_bot3">
+              <figure class="left marg_right1"><img src="images/page1_img4.jpg" alt=""></figure>
+              <p class="pad_bot1"><strong class="color2">2017, 3 baths, 6 beds, building size: 5000 sq. ft.<br>
+                Price: <span class="color1">$US 600 000</span></strong></p>
+              <p class="pad_bot2"> PRICE REDUCED!...AN EXCELLENT FIXER-UPPER! A Multi-family property with five sections on property 
+			  total 9 beds & 6bath.  Excellent income generating property. Prime location Hughenden off Molynes. Close to all major supermarkets, 
+			  schools and transportation. PROPERTY TENANTED SO VERY LIMITED VIEWING.</p>
+              <br><a href="#" class="button">Read more</a> </div>
+            <div class="wrapper">
+              <figure class="left marg_right1"><img src="images/page1_img5.jpg" alt=""></figure>
+              <p class="pad_bot1"><strong class="color2">2017, 6 baths, 4 beds, building size: 8000 sq. ft.<br>
+                Price: <span class="color1">$US 300 000</span></strong></p>
+              <p class="pad_bot2"> Spacious well laid out two storey four bedroom four bathroom house in quiet cul de sac in Forest Hills. 
+			  Additionally, indoor laundry room and large family room and entertainment section on ground floor. No reasonable offer will be refused 
+			  - motivated seller..</p>
+              <a href="#" class="button">Read more</a> </div>
+          </div>
+        </article>
+        <article class="col2">
+          <div class="pad1">
+            <h3>Recent News</h3>
+            <div class="wrapper"> <span class="date"><strong>28</strong><span>AUG</span></span>
+              <p><a href="#" class="color2">RESIDENTIAL LOT</a><br>
+                KINGSTON & ST. ANDREW, AIRY CASTLE, STONY HILL Residential lot, 3 1/4 acres... <a href="#">more</a></p>
+            </div>
+            <div class="wrapper"> <span class="date"><strong>25</strong><span>SEP</span></span>
+              <p><a href="#" class="color2">House</a><br>
+                A developer or investor's dream..An incomplete multi-storey structure with a basement. All three floors/apts designed to be fully self-contained... <a href="#">more</a></p>
+            </div>
+            <div class="wrapper"> <span class="date"><strong>22</strong><span>may</span></span>
+              <p><a href="#" class="color2">House</a><br>
+                A lovely two-family house with the following facilities- Small side 2 bedrooms, 1 bathroom, living room, kitchen, small back porch/laundry area, enclosed... <a href="#">more</a></p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
+</div>
+<!-- / content -->
+<div class="body4">
+  <div class="main">
+    <!-- footer -->
+    <footer> <span class="call">For your design call: <span>1-876-477-2117</span></span> Copyright &copy; <a href="#">MI CASA 2017</a> All Rights Reserved<br>
+      Design by <a target="_blank" href=""> Mikhail Gooden Helium</a>
+      
+    </footer>
+    <!-- / footer -->
+  </div>
+</div>
+
+</body>
+</html>
